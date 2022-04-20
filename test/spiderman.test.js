4075e7c9226496f9e7597f07285ac2118b39851c
DianaMagallanes <dianakarina2003@example.com>
2022-04-19 19:41:45 -0700

    Se modifican archivos para agregar atributos

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -6,7 +6,7 @@ describe("Unit Tests for Spiderman class", () => {
       expect(andrewGarfield.name).toBe("Spiderman Sony");
       expect(andrewGarfield.age).toBe(31);
       expect(andrewGarfield.actor).toBe("Andrew Garfield");
-      expect(andrewGarfield.actor).toBe(2);
+      expect(andrewGarfield.movies).toBe(2);
       expect(andrewGarfield.actor).toBe("Sony");
     });
   })
